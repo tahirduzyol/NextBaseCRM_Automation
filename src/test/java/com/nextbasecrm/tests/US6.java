11006e5c2a3db0a9a9d21e9651d61e1f7f3f7df1
ozgechengo <93153305+ozgechengo@users.noreply.github.com>
2022-02-28 16:21:47 -0500

    US6 Codes added +

--- src/test/java/com/nextbasecrm/tests/US6.java
+++ src/test/java/com/nextbasecrm/tests/US6.java
@@ -2,11 +2,13 @@
 package com.nextbasecrm.tests;
 
 import com.nextbasecrm.utilities.CRM_Utilities;
+import com.nextbasecrm.utilities.ConfigurationReader;
 import com.nextbasecrm.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -20,15 +22,22 @@ public class US6 {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://login1.nextbasecrm.com");
-        CRM_Utilities.crm_login(driver, "hr46@cydeo.com", "UserUser");
+        driver.get(ConfigurationReader.getProperty("env"));
+
 
     }
+    @DataProvider(name = "logins")
+    public Object[][] logins(){
+        return new Object[][] { {"hr46@cydeo.com"}, {"helpdesk46@cydeo.com"}, {"marketing46@cydeo.com"},
+                {"hr47@cydeo.com"}, {"helpdesk47@cydeo.com"}, {"marketing47@cydeo.com"},
+                {"hr48@cydeo.com"}, {"helpdesk48@cydeo.com"}, {"marketing48@cydeo.com"} };
+    }
 
-    @Test
 
-    public void project() {
+    @Test(dataProvider = "logins")
 
+    public void project(String login) {
+        CRM_Utilities.crm_login(driver,login  , "UserUser");
 
         WebElement moreButton = driver.findElement(By.xpath("//span[@id='feed-add-post-form-link-text']"));
         System.out.println("More BUTTON is displayed " + moreButton.isDisplayed());
@@ -46,7 +55,11 @@ public class US6 {
         WebElement popupbutton4 = driver.findElement(By.xpath("(//span[@class='menu-popup-item-text'])[4]"));
         System.out.println("Pop up button 4 (Workflow)  is displayed  = " + popupbutton4.isDisplayed());
 
+        driver.quit();
 
     }
-}
 
+
+
+
+}
